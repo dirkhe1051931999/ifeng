4b9d9e58c4dc0f58ead7659c26e533b4b1190c4e
dirkhe1051931999 <hejiandirk@163.com>
2024-11-27 16:14:12 +0800

    add app repository

--- proxy/api/ifeng_home_tab_xiaoshuo.js
+++
@@ -1,8 +0,0 @@
-module.exports = function init(app) {
-  const ifeng_tab_xiaoshuo_index = require('../html_parser/home_tab_xiaoshuo/index');
-  // const ifeng_tab_yingshiju_dajurebo = require('../html_parser/home_tab_yingshiju/dajurebo');
-  app.get('/ifeng_web/home_tab_xiaoshuo/index', async (req, res) => {
-    const result = await ifeng_tab_xiaoshuo_index(req.query);
-    res.send(result);
-  });
-};
