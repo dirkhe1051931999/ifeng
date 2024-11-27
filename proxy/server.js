e5bac3a37fcfe9f56ce89db43057e1db613687c0
dirkhe1051931999 <hejiandirk@163.com>
2024-11-27 14:10:55 +0800

    chore(proxy): Remove unused static file serving
    Remove unnecessary static file serving functionality from the proxy server.
    
    - Removed the `express.static` middleware.
    - Simplified the server code.
    - Improved server efficiency.

--- proxy/server.js
+++ proxy/server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const randomUseragent = require('random-useragent');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-const path = require('path');
 require('./api/ifeng_home_tab_kangyi')(app); // 抗疫
 require('./api/ifeng_home_tab_caijing')(app); // 财经
 require('./api/ifeng_home_tab_yingshiju')(app); // 影视剧
 require('./api/ifeng_home_tab_keji')(app); // 科技
 require('./api/ifeng_home_tab_xiaoshuo')(app); // 小说
-// static file
-app.use('/static', express.static(path.join(__dirname, 'public')));
 // https://nine.ifeng.com
 app.use(
   '/',
