64a2fa96eb0e564becce8be52d31f51f9e778cd0
dirkhe1051931999 <hejiandirk@163.com>
2025-03-20 20:56:14 +0800

    feat(config): enhance PM2 configuration with logging and cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,6 +11,10 @@ module.exports = {
     },
     autorestart: true,
     max_restarts: 10,
-    min_uptime: "1m"
+    min_uptime: "1m",
+    exec_mode: 'cluster',
+    out_file: './logs/out.log', // 指定标准输出日志文件
+    error_file: './logs/error.log', // 指定错误输出日志文件
+    log_date_format: 'YYYY-MM-DD HH:mm Z', // 日志时间格式
   }]
 }
